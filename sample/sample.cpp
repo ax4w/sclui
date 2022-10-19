745ceaeb31b39b7c44726e0fb7a8cd9faca87b1f
0x3Alex <a-wrede@outlook.de>
2022-10-19 18:55:54 +0200

    comments

--- sample/sample.cpp
+++ sample/sample.cpp
@@ -2,7 +2,7 @@
 
 using namespace sclui;
 //global scope to access it in extOnV
-TextBox *textbox1 = nullptr; //public to pass into the event function
+static TextBox *textbox1 = nullptr; //public to pass into the event function
 
 void returnOnV(int c) {
     if(c == 'v') return;
@@ -20,11 +20,13 @@ int main(void) {
 
     //doQuit will free every item on that screen
     //every screen will be freed when a new screen is loaded
-    button1->onButtonPress = &doQuit; //register press event
+    button1->onButtonPress = &doQuit; //register press event(
     Text *text1 = new Text("Hallo welt",1,3,COLOR_BLANK);
     CheckBox *checkbox1 = new CheckBox("check",1,6,COLOR_CYAN, COLOR_RED,false);
     checkbox1->onCheckBoxChange = &doQuit; //register checkbox change event
     textbox1 =  new TextBox("text",1,9,5,COLOR_BLUE, COLOR_GREEN, &TextBoxFilterIsNumber);
+
+
     textbox1->onKeyPress = &returnOnV; //register key press event 
     
     //add items
